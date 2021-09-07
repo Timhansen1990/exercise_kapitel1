3cacb0bb75d98cbc9144dbb42174c2f2743915ec
tim Hansen <timx1776@edu.easj.dk>
2021-09-07 18:11:28 +0200

    Chapter3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,49 @@
 package com.company;
-
+import javax.sound.sampled.Line;
+import java.util.Scanner;
+/* here we are importing the class scanner, scanner is so the editor know we are talking about,
+   strings, variables etc.*/
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Hello world");
+        //System.out.println(System.out);//
+        // This code is showing information about the class system.out //
+
+
+
+        /*String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Type something: ");
+        line = in.nextLine();
+        System.out.println("You said: " + line);
+
+        System.out.print("Type something else: ");
+        line = in.nextLine();
+        System.out.println("You also said: " + line);
+
+        */
+
+
+
+
+       /* int inch;
+        double cm;
+        final double CM_PER_INCH = 2.54;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many inches? ");
+        inch = in.nextInt();
+        cm = inch * CM_PER_INCH;
+        System.out.print(inch + " in = ");
+        System.out.println(cm + " cm");
+        */
+       // System.out.print(4.0/ 3.0); //
+        // Here there will be print out many decimals//
+                        System.out.println("ghhhg");
+
+
     }
 }
