56c6aeb7b9cc709bd4458f774c2ebb781838dca5
Timhansen1990 <timx1776@edu.easj.dk>
2021-09-08 08:54:41 +0200

    Chapter3_excercise1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// exercise 1
+        Scanner Scanner = new Scanner(System.in);
 
-        System.out.println("Hello world");
+        System.out.print("Enter a temperature in Celsius: ");
+        double Celsius = Scanner.nextDouble();
+
+        double Fahrenheit = Celsius * 9 / 5 + 32;
+
+        System.out.printf("%.1f C = %.1f F\n", Celsius, Fahrenheit);
+
+
+
     }
 }
