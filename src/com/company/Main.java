4b17d973c818a44d5aabcc48e00c460946e29d91
tim Hansen <timx1776@edu.easj.dk>
2021-09-08 07:42:38 +0200

    Chapter3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,51 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int C;
+        Scanner F = new Scanner(System.in);
+        System.out.print("Type Celsius: ");
 
-        //System.out.println(System.out);//
-        // This code is showing information about the class system.out //
+        C = F.nextLine();
 
-
-
-        /*String line;
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Type something: ");
-        line = in.nextLine();
-        System.out.println("You said: " + line);
-
-        System.out.print("Type something else: ");
-        line = in.nextLine();
-        System.out.println("You also said: " + line);
-
-        */
-
-
-
-
-       /* int inch;
-        double cm;
-        final double CM_PER_INCH = 2.54;
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("How many inches? ");
-        inch = in.nextInt();
-        cm = inch * CM_PER_INCH;
-        System.out.print(inch + " in = ");
-        System.out.println(cm + " cm");
-
-       System.out.print(4.0/ 3.0);
-       Here there will be print out many decimals
-        */
-        //System.out.printf("Four Thirds = %.3f", 4.0 / 3.0);//
-       /* int inch = 80;
-        double CM_PR_INCH = 2.54;
-        double cm = inch * CM_PR_INCH;
-        System.out.printf("%d in = %f cm\n", inch, cm);
-        */
-
-        /* double pi = 3.14159;
-           int x = (int) pi;*/
 
     }
 }
